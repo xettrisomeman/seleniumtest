149e329b2f0ac5d6e648901d8564f14771a73ae1
webizfabulous <webizmoxx@gmail.com>
2019-11-06 12:57:57 +0545

    Added facebook

--- test_file1.py
+++ test_file1.py
@@ -4,11 +4,20 @@ from selenium import webdriver
 
 class TestSelenium(unittest.TestCase):
 
-    def test_one(self):
+    #test google title
+    def test_title(self):
         driver = webdriver.Firefox()
         driver.get("https://google.com")
         title = driver.title 
         driver.close()
         self.assertEqual(title , "Google")
         
+    #test faceboook h2 element text
+    def test_h2_element(self):
+        driver = webdriver.Firefox()
+        driver.get("https://facebook.com")
+        elem = driver.find_element_by_tag_name("h2").text
+        driver.close()
+        self.assertFalse(elem)
+
 
